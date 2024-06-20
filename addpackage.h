4d26e4579e27a7b55c7159e09ba85c74668a1616
sagarika0309 <155207561+sagarika0309@users.noreply.github.com>
2024-06-20 18:35:20 +0530

    Add files via upload

---
+++ addpackage.h
@@ -0,0 +1,32 @@
+//Author: Cppbuzz.com
+//Note: Users can download & modify this project as per their requirement
+
+#ifndef ADDPACKAGE_H
+#define ADDPACKAGE_H
+
+#include <QDialog>
+#include "databaseheader.h"
+
+namespace Ui {
+class AddPackage;
+}
+
+class AddPackage : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AddPackage(QWidget *parent = nullptr);
+    ~AddPackage();
+
+private slots:
+    void on_btnSave_clicked();
+
+    void on_btnReset_clicked();
+
+private:
+    Ui::AddPackage *ui;
+    QSqlDatabase database;
+};
+
+#endif // ADDPACKAGE_H
