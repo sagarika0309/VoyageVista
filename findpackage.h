eee657b759bf5cfa4e2dd5c4901e5a34582b14c8
sagarika0309 <155207561+sagarika0309@users.noreply.github.com>
2024-06-20 18:34:38 +0530

    Add files via upload

---
+++ findpackage.h
@@ -0,0 +1,30 @@
+//Author: Cppbuzz.com
+//Note: Users can download & modify this project as per their requirement
+
+#ifndef FINDPACKAGE_H
+#define FINDPACKAGE_H
+
+#include <QDialog>
+#include "databaseheader.h"
+
+namespace Ui {
+class FindPackage;
+}
+
+class FindPackage : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FindPackage(QWidget *parent = nullptr);
+    ~FindPackage();
+
+private slots:
+    void on_btnFindPackage_clicked();
+
+private:
+    Ui::FindPackage *ui;
+    QSqlQueryModel *model;
+};
+
+#endif // FINDPACKAGE_H
---
+++ findtourist.h
@@ -0,0 +1,30 @@
+//Author: Cppbuzz.com
+//Note: Users can download & modify this project as per their requirement
+
+#ifndef FINDTOURIST_H
+#define FINDTOURIST_H
+
+#include <QDialog>
+#include "databaseheader.h"
+
+namespace Ui {
+class FindTourist;
+}
+
+class FindTourist : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FindTourist(QWidget *parent = nullptr);
+    ~FindTourist();
+
+private slots:
+    void on_btnFind_clicked();
+
+private:
+    Ui::FindTourist *ui;
+     QSqlQueryModel *model;
+};
+
+#endif // FINDTOURIST_H
